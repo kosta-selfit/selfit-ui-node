d56b28d8249c7a6f93a125b55c18c48ff86d5b10
kokoko12334 <gnjgnjgnj1@gmail.com>
2025-06-12 15:42:28 +0900

    firstcommit

---
+++ src/main/java/com/oopsw/selfitui/SelfituiApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.selfitui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfituiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfituiApplication.class, args);
+	}
+
+}
